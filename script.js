15e0ab85ae5d3b8b2b55e073c4cba1bd8e3e218b
Juan Gonzalez <john256145@gmail.com>
2021-11-27 17:03:28 -0600

    added test api code

--- script.js
+++ script.js
@@ -15,3 +15,58 @@ function loginButton() {
 
 
 }
+
+function fetchUsers() {
+    var url = "https://api.airtable.com/v0/appO1nRBNkCmnuuCB/Users?maxRecords=3&view=Grid%20view";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url);
+
+xhr.setRequestHeader("Authorization", "Bearer " + airtableApiKey);
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+xhr.send();
+}
+
+
+function addDummyUser() {
+    var url = "https://api.airtable.com/v0/appO1nRBNkCmnuuCB/Users";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Authorization", "Bearer " + airtableApiKey);
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = `{
+  "records": [
+    {
+      "fields": {
+          "firstname": "Jane",
+          "lastname": "Doe",
+          "address": "4321 North Ave",
+          "phone": "7735550000",
+          "email": "idk@example.com",
+          "password": "password",
+          "points": 0,
+          "creditcard": "1738",
+          "isadmin": false
+      }
+    }
+  ]
+}`;
+
+xhr.send(data);
+
+}
