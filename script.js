6346b939b679dca1f763ff27b1a5bf2e9514028b
Juan Gonzalez <john256145@gmail.com>
2021-11-27 17:46:35 -0600

    user sign up implemented

--- script.js
+++ script.js
@@ -16,6 +16,19 @@ function loginButton() {
 
 }
 
+function addUserfromForm() {
+    var firstName=document.getElementById("firstNameInput").value;
+    var lastName = document.getElementById("lastNameInput").value;
+    var email = document.getElementById("inputEmail3").value;
+    var password = document.getElementById("inputPassword3").value;
+    var cc = document.getElementById("creditCardNumber").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var address = document.getElementById("inputAddress").value;
+    var isAdmin = document.getElementById("gridRadios2").checked;
+    addUser(firstName, lastName, email, password, cc, phoneNumber, address, isAdmin);
+    alert("Welcome, " + firstName + ". You may now log in.");
+}
+
 function fetchUsers() {
     var url = "https://api.airtable.com/v0/appO1nRBNkCmnuuCB/Users?maxRecords=3&view=Grid%20view";
 
@@ -33,6 +46,41 @@ xhr.onreadystatechange = function () {
 xhr.send();
 }
 
+function addUser(firstName, lastName, email, password, cc, phoneNumber, address, isAdmin) {
+    var url = "https://api.airtable.com/v0/appO1nRBNkCmnuuCB/Users";
+
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+xhr.setRequestHeader("Authorization", "Bearer " + airtableApiKey);
+xhr.setRequestHeader("Content-Type", "application/json");
+
+xhr.onreadystatechange = function () {
+   if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+   }};
+
+var data = `{
+  "records": [
+    {
+      "fields": {
+          "firstname": "` + firstName + `",
+          "lastname": "`+ lastName +`",
+          "address": "` + address + `",
+          "phone": "` + phoneNumber + `",
+          "email": "` + email + `",
+          "password": "` + password+ `",
+          "points": 0,
+          "creditcard": "` + cc + `",
+          "isadmin": ` + isAdmin + `
+      }
+    }
+  ]
+}`;
+
+xhr.send(data);
+}
 
 function addDummyUser() {
     var url = "https://api.airtable.com/v0/appO1nRBNkCmnuuCB/Users";
